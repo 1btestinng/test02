c1523a7adb877f0117ecce062e48cdf22c4000c7
ahzcapital <ahzcapital01@gmail.com>
2026-09-11 19:03:34 +0300

    refactor: replace conflicting company routes

--- app/company/[ticker]/page.tsx
+++
@@ -1,6 +0,0 @@
-import {redirect} from 'next/navigation';
-
-export default async function CompanyPage({params}:{params:Promise<{ticker:string}>}){
-  const {ticker}=await params;
-  redirect(`/company/EG/${encodeURIComponent(ticker)}`);
-}
